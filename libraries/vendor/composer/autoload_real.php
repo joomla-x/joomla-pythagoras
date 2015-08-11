43708829e7f25686918cbb2abfd4559e5a02e279
Niels Braczek <nbraczek@bsds.de>
2015-08-11 16:07:54 +0200

    Required Joomla\Session 2.0

--- libraries/vendor/composer/autoload_real.php
+++ libraries/vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInitf9aed076f12471aff0049364a504b3f7
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf9aed076f12471aff0049364a504b3f7', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
