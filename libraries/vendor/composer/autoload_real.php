cda0c6d58b3cdc157be49d83f10edee0df60b7b5
Niels Braczek <nbraczek@bsds.de>
2015-08-14 14:45:31 +0200

    Added dev dependencies again (also to .gitignore)

--- libraries/vendor/composer/autoload_real.php
+++ libraries/vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInitb78c84d8c3d9c753c6deea86b9f96f76
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitb78c84d8c3d9c753c6deea86b9f96f76', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
