e633658b044bc32723fc54cb70c4c848e3b02487
Michael Babker <mbabker@flbab.com>
2015-08-23 23:34:31 -0400

    Remove stub log, dispatcher and database classes

--- libraries/import.legacy.php
+++ libraries/import.legacy.php
@@ -85,6 +85,5 @@ JLoader::register('PasswordHash', JPATH_PLATFORM . '/phpass/PasswordHash.php');
 JLoader::register('JSimpleCrypt', JPATH_PLATFORM . '/legacy/simplecrypt/simplecrypt.php');
 JLoader::register('JTree', JPATH_PLATFORM . '/legacy/base/tree.php');
 JLoader::register('JNode', JPATH_PLATFORM . '/legacy/base/node.php');
-JLoader::register('LogException', JPATH_PLATFORM . '/legacy/log/logexception.php');
 JLoader::register('JXMLElement', JPATH_PLATFORM . '/legacy/utilities/xmlelement.php');
 JLoader::register('JApplication', JPATH_LIBRARIES . '/legacy/application/application.php');
