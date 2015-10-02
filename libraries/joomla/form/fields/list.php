e261ebaae41d1acb93ad72fff4598eb89c30ee99
Daniele Rosario <realskullbock@gmail.com>
2015-10-02 18:49:50 +0200

    First stub of new JformField using JLayout

--- libraries/joomla/form/fields/list.php
+++ libraries/joomla/form/fields/list.php
@@ -36,31 +36,17 @@ class JFormFieldList extends JFormField
 	protected function getInput()
 	{
 		$html = array();
-		$attr = '';
 
-		// Initialize some field attributes.
-		$attr .= !empty($this->class) ? ' class="' . $this->class . '"' : '';
-		$attr .= !empty($this->size) ? ' size="' . $this->size . '"' : '';
-		$attr .= $this->multiple ? ' multiple' : '';
-		$attr .= $this->required ? ' required aria-required="true"' : '';
-		$attr .= $this->autofocus ? ' autofocus' : '';
-
-		// To avoid user's confusion, readonly="true" should imply disabled="true".
-		if ((string) $this->readonly == '1' || (string) $this->readonly == 'true' || (string) $this->disabled == '1'|| (string) $this->disabled == 'true')
-		{
-			$attr .= ' disabled="disabled"';
-		}
-
-		// Initialize JavaScript field attributes.
-		$attr .= $this->onchange ? ' onchange="' . $this->onchange . '"' : '';
-
-		// Get the field options.
-		$options = (array) $this->getOptions();
+		// Create a regular list.
+		$displayData = (array) $this;
+		$displayData['options'] = $this->getOptions();
 
 		// Create a read-only list (no name) with hidden input(s) to store the value(s).
 		if ((string) $this->readonly == '1' || (string) $this->readonly == 'true')
 		{
-			$html[] = JHtml::_('select.genericlist', $options, '', trim($attr), 'value', 'text', $this->value, $this->id);
+			$displayData['readonly'] = true;
+			$displayData['name'] = '';
+			$displayData['hiddenName'] = $this->name;
 
 			// E.g. form field type tag sends $this->value as array
 			if ($this->multiple && is_array($this->value))
@@ -69,24 +55,10 @@ class JFormFieldList extends JFormField
 				{
 					$this->value[] = '';
 				}
-
-				foreach ($this->value as $value)
-				{
-					$html[] = '<input type="hidden" name="' . $this->name . '" value="' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '"/>';
-				}
-			}
-			else
-			{
-				$html[] = '<input type="hidden" name="' . $this->name . '" value="' . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"/>';
 			}
 		}
-		else
-		// Create a regular list.
-		{
-			$html[] = JHtml::_('select.genericlist', $options, $this->name, trim($attr), 'value', 'text', $this->value, $this->id);
-		}
-
-		return implode($html);
+		
+		return JLayoutHelper::render('joomla.form.field.list', $displayData);
 	}
 
 	/**
