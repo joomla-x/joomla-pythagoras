15bb1116d62839fbd0c842a4710086b7a9d8226d
George Wilson <georgejameswilson@googlemail.com>
2015-10-26 22:51:58 +0000

    More missing JHtml calls

--- components/com_contact/views/categories/tmpl/default.php
+++ components/com_contact/views/categories/tmpl/default.php
@@ -9,6 +9,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Html\Html as JHtml;
 use Joomla\CMS\Layout\Helper as JLayoutHelper;
 
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
--- components/com_contact/views/contact/tmpl/default.php
+++ components/com_contact/views/contact/tmpl/default.php
@@ -10,6 +10,7 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Component\Helper as JComponentHelper;
+use Joomla\CMS\Html\Html as JHtml;
 use Joomla\CMS\Layout\File as JLayoutFile;
 use Joomla\CMS\Plugin\Helper as JPluginHelper;
 
