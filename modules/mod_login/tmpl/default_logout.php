4b528da46216575ffb2203b49c66e9af6ed485e8
George Wilson <georgejameswilson@googlemail.com>
2015-10-27 20:45:10 +0000

    Fix missing JHtml call in mod_login

--- modules/mod_login/tmpl/default_logout.php
+++ modules/mod_login/tmpl/default_logout.php
@@ -9,6 +9,8 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Html\Html as JHtml;
+
 JHtml::_('behavior.keepalive');
 ?>
 <form action="<?php echo JRoute::_(htmlspecialchars(JUri::getInstance()->toString()), true, $params->get('usesecure')); ?>" method="post" id="login-form" class="form-vertical">
