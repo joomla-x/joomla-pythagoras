186cea8d3a08fed4ee3888a9dbd6f5ad23b97c80
George Wilson <georgejameswilson@googlemail.com>
2015-10-27 21:06:36 +0000

    Fix missing JHtml calls in com_users

--- components/com_users/helpers/html/users.php
+++ components/com_users/helpers/html/users.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die;
  *
  * @since  1.6
  */
-abstract class JHtmlUsers
+abstract class UsersHtmlUsers
 {
 	/**
 	 * Get the sanitized value
--- components/com_users/views/profile/tmpl/default_params.php
+++ components/com_users/views/profile/tmpl/default_params.php
@@ -11,13 +11,13 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Html\Html as JHtml;
 
-JLoader::register('JHtmlUsers', JPATH_COMPONENT . '/helpers/html/users.php');
-JHtml::register('users.spacer', array('JHtmlUsers', 'spacer'));
-JHtml::register('users.helpsite', array('JHtmlUsers', 'helpsite'));
-JHtml::register('users.templatestyle', array('JHtmlUsers', 'templatestyle'));
-JHtml::register('users.admin_language', array('JHtmlUsers', 'admin_language'));
-JHtml::register('users.language', array('JHtmlUsers', 'language'));
-JHtml::register('users.editor', array('JHtmlUsers', 'editor'));
+JLoader::register('UsersHtmlUsers', JPATH_COMPONENT . '/helpers/html/users.php');
+JHtml::register('usersHtml.users.spacer', array('UsersHtmlUsers', 'spacer'));
+JHtml::register('usersHtml.users.helpsite', array('UsersHtmlUsers', 'helpsite'));
+JHtml::register('usersHtml.users.templatestyle', array('UsersHtmlUsers', 'templatestyle'));
+JHtml::register('usersHtml.users.admin_language', array('UsersHtmlUsers', 'admin_language'));
+JHtml::register('usersHtml.users.language', array('UsersHtmlUsers', 'language'));
+JHtml::register('usersHtml.users.editor', array('UsersHtmlUsers', 'editor'));
 
 ?>
 <?php $fields = $this->form->getFieldset('params'); ?>
@@ -29,14 +29,14 @@ JHtml::register('users.editor', array('JHtmlUsers', 'editor'));
 		if (!$field->hidden) :?>
 		<dt><?php echo $field->title; ?></dt>
 		<dd>
-			<?php if (JHtml::isRegistered('users.' . $field->id)):?>
-				<?php echo JHtml::_('users.' . $field->id, $field->value);?>
-			<?php elseif (JHtml::isRegistered('users.' . $field->fieldname)):?>
-				<?php echo JHtml::_('users.' . $field->fieldname, $field->value);?>
-			<?php elseif (JHtml::isRegistered('users.' . $field->type)):?>
-				<?php echo JHtml::_('users.' . $field->type, $field->value);?>
+			<?php if (JHtml::isRegistered('usersHtml.users.' . $field->id)):?>
+				<?php echo JHtml::_('usersHtml.users.' . $field->id, $field->value);?>
+			<?php elseif (JHtml::isRegistered('usersHtml.users.' . $field->fieldname)):?>
+				<?php echo JHtml::_('usersHtml.users.' . $field->fieldname, $field->value);?>
+			<?php elseif (JHtml::isRegistered('usersHtml.users.' . $field->type)):?>
+				<?php echo JHtml::_('usersHtml.users.' . $field->type, $field->value);?>
 			<?php else:?>
-				<?php echo JHtml::_('users.value', $field->value);?>
+				<?php echo JHtml::_('usersHtml.users.value', $field->value);?>
 			<?php endif;?>
 		</dd>
 		<?php endif;?>
