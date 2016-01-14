251b7ad5ed7c503065d86741f6b1bc17308c6005
Niels Braczek <nbraczek@bsds.de>
2016-01-14 13:28:19 +0100

    Changed working directory

--- tests/acceptance/runtests.sh
+++ tests/acceptance/runtests.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+cd tests/acceptance
+
 docker-compose up -d
 #docker-compose scale firefox=5
 sleep 1
 
-CURRENT=`pwd`
-
 cd ../..
 
-./libraries/vendor/bin/codecept run -c codeception.yml.dist acceptance
+libraries/vendor/bin/codecept run -c codeception.yml.dist acceptance
 
-cd $CURRENT
+cd tests/acceptance
 
 docker-compose stop
 docker-compose rm --force
+
+cd ../..
