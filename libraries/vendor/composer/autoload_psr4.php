2e99bd9e93188e2e8eb57349f6397e10c79ce106
Niels Braczek <nbraczek@bsds.de>
2016-02-04 18:47:28 +0100

    Forced empty directories to be included

--- libraries/vendor/composer/autoload_psr4.php
+++ libraries/vendor/composer/autoload_psr4.php
@@ -35,7 +35,7 @@ return array(
     'Joomla\\String\\' => array($vendorDir . '/joomla/string/src'),
     'Joomla\\Registry\\Tests\\' => array($vendorDir . '/joomla/registry/Tests'),
     'Joomla\\Registry\\' => array($vendorDir . '/joomla/registry/src'),
-    'Joomla\\' => array($baseDir . '/libraries'),
+    'Joomla\\' => array($baseDir . '/libraries/incubator'),
     'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
