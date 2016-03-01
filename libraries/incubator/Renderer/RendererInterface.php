7b27ccb2100c75f32fd35ba581ff27e547357d9b
Niels Braczek <nbraczek@bsds.de>
2016-03-01 15:32:11 +0100

    Introduced RendererInterface

---
+++ libraries/incubator/Renderer/RendererInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Part of the Joomla Framework Renderer Package
+ *
+ * @copyright  Copyright (C) 2015 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Renderer;
+
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Renderer Interface
+ *
+ * @package  Joomla/renderer
+ * @since    1.0
+ */
+interface RendererInterface extends StreamInterface
+{
+	/**
+	 * @param   string   $type     The content type
+	 * @param   callable $handler  The handler for that type
+	 *
+	 * @return  void
+	 */
+	public function registerContentType($type, callable $handler);
+}
