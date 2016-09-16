c19a82a005c9cdabfab8af48a07e4fc784cc3316
nibra <nbraczek@bsds.de>
2016-09-16 15:27:13 +0200

    Preparation for alternative CSS frameworks: Templates

---
+++ templates/uikit2/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of the Joomla UIKit Frontend Package
+ *
+ * @copyright  Copyright (C) 2015 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ *
+ * @var   string $title The page title
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title><?php echo $title; ?> - Frontend</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js"></script>	</head>
+	<body></body>
+</html>
