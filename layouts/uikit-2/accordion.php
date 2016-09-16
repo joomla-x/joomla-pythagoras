e594df38ae19719b82666d33731051596bd5bfc3
Ufuk Avcu <ufuk@jooag.de>
2016-09-16 19:03:26 +0200

    Update accordion.php (#18)

--- layouts/uikit-2/accordion.php
+++ layouts/uikit-2/accordion.php
@@ -11,20 +11,9 @@
 
 $class = $content->params->class ?? '';
 ?>
-<div class="panel-group <?php echo $class; ?>" id="<?php echo $content->id; ?>">
+<div class="uk-accordion  <?php echo $class; ?>"  id="<?php echo $content->id; ?>" data-uk-accordion>
 	<?php foreach ($content->elements as $i => $element) : ?>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h4 class="panel-title">
-				<a data-toggle="collapse" data-parent="#<?php echo $content->id; ?>" href="#<?php echo $content->id . '-' . $i; ?>">
-					<?php echo $element->title; ?></a>
-			</h4>
-		</div>
-		<div id="<?php echo $content->id . '-' . $i; ?>" class="panel-collapse collapse<?php echo $i == 0 ? ' in' : ''; ?>">
-			<div class="panel-body">
-				<?php echo $element->html; ?>
-			</div>
-		</div>
-	</div>
+		<h3 class="uk-accordion-title"><?php echo $element->title; ?></h3>
+    		<div class="uk-accordion-content"><?php echo $element->html; ?></div>
 	<?php endforeach; ?>
 </div>
