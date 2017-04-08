aa0df390eab7372438f53dbeb3209142f526727b
nibra <nbraczek@bsds.de>
2017-04-08 02:54:31 +0200

    Fixed CLI container

--- build/docker/cli/runtests.sh
+++ build/docker/cli/runtests.sh
@@ -8,11 +8,10 @@ cd $WORK_DIR
 cp /usr/local/lib/php/prepend.php .
 cp /usr/local/lib/php/append.php .
 cp /usr/local/lib/php/phpunit_coverage.php .
-cp tests/cli/codeception.yml .
 
 echo "Running tests ..."
 
-./libraries/vendor/bin/codecept run --coverage=coverage.cli.php cli
+./libraries/vendor/bin/robo test:cli --coverage
 return_code=$?
 
 echo "Collecting results ..."
@@ -22,7 +21,6 @@ php phpunit_coverage.php
 rm prepend.php
 rm append.php
 rm phpunit_coverage.php
-rm codeception.yml
 
 echo "Done."
 
